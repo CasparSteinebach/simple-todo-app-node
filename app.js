21b8b362f9cf29872b88782594fd1628ba8cdf0f
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-02 15:59:45 +0100

    seperated the routes and endpoint from app.js

--- app.js
+++ app.js
@@ -1,144 +1,15 @@
 const express = require('express');
 const db = require('./db/db');
 const bodyParser = require('body-parser');
-// Set up the express app
+const router = require('./routes/index')
+    // Set up the express app
 const app = express();
 
 // parse incoming  request data using bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-////// Endpoints go here //////
-
-// get all todos
-
-app.get('/api/v1/todos', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'todos retrieved successfully',
-        todos: db
-    })
-});
-
-//get a todo by id
-
-app.get('/api/v1/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    db.map((todo) => {
-        if (todo.id === id) {
-            return res.status(200).send({
-                success: 'true',
-                message: 'todo retrieved successfully',
-                todo,
-            });
-        }
-    });
-    return res.status(404).send({
-        success: 'false',
-        message: 'todo does not exist',
-    });
-});
-
-
-// post a new task to the todo list
-
-//first check if the post fits the requirements for the todo list
-app.post('/api/v1/todos', (req, res) => {
-    if (!req.body.title) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'title is required'
-        });
-    } else if (!req.body.description) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'description is required'
-        });
-    }
-    //constructor for the post in the todo
-    const todo = {
-            id: db.length + 1,
-            title: req.body.title,
-            description: req.body.description
-        }
-        // push the todo to the db
-    db.push(todo);
-    return res.status(201).send({
-        success: 'true',
-        message: 'todo added successfully',
-        todo
-    })
-});
-
-// delete a todo from te list by id
-
-app.delete('/api/v1/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-
-    db.map((todo, index) => {
-        if (todo.id === id) {
-            db.splice(index, 1);
-            return res.status(200).send({
-                success: 'true',
-                message: 'Todo deleted successfuly',
-            });
-        }
-    });
-
-    return res.status(404).send({
-        success: 'false',
-        message: 'todo not found',
-    });
-
-
-});
-
-// change a todo by id
-
-app.put('/api/v1/todos/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    let todoFound;
-    let itemIndex;
-    db.map((todo, index) => {
-        if (todo.id === id) {
-            todoFound = todo;
-            itemIndex = index;
-        }
-    });
-
-    if (!todoFound) {
-        return res.status(404).send({
-            success: 'false',
-            message: 'todo not found',
-        });
-    }
-
-    if (!req.body.title) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'title is required',
-        });
-    } else if (!req.body.description) {
-        return res.status(400).send({
-            success: 'false',
-            message: 'description is required',
-        });
-    }
-
-    const updatedTodo = {
-        id: todoFound.id,
-        title: req.body.title || todoFound.title,
-        description: req.body.description || todoFound.description,
-    };
-
-    db.splice(itemIndex, 1, updatedTodo);
-
-    return res.status(201).send({
-        success: 'true',
-        message: 'todo added successfully',
-        updatedTodo,
-    });
-});
 
 ////Set PORT and listener////
 
